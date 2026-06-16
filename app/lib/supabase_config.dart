a959374cb30936ab06be17b15bbe8a85b10ce208
Faybeata <130736804+faybeez@users.noreply.github.com>
2026-06-16 17:33:10 +0800

    chore: delete supabase local config

--- app/lib/supabase_config.dart
+++
@@ -1,6 +0,0 @@
-const supabaseUrl = 'http://192.168.0.101:54321';
-const supabasePublishableKey = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH';
-
-bool get hasSupabaseConfig =>
-    !supabaseUrl.startsWith('YOUR_') &&
-    !supabasePublishableKey.startsWith('YOUR_');
